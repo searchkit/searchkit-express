96a42910d95ec1aca97c1ffc6fc787a817cb474d
Joseph McElroy <joseph.mcelroy@elastic.co>
2023-03-04 21:34:38 +0000

    update to v4

--- README.md
+++ README.md
@@ -1,66 +1,27 @@
 ### Searchkit Express
-A thin library to create an express to elasticsearch proxy to support the searchkit ui framework
 
-```sh
-npm install searchkit-express --save
-```
+Example of using Searchkit with Express.js to proxy frontend search requests to Elasticsearch.
 
-```js
-var SearchkitExpress = require("searchkit-express")
-```
+### Setup
 
-#### Add _search endpoint to root url
-if you just want to create a proxy to the root express url then use the `SearchkitExpress` function directly like this
-```js
-var app = express()
-app.use(bodyParser.json())
-//...
-
-SearchkitExpress({
-  // For HTTP basic auth use format: "http://username:password@localhost:9200" in host parameter
-  host:process.env.ELASTIC_URL || "http://localhost:9200", 
-  index:'movies',
-  queryProcessor:function(query, req, res){
-    //do neccessery permissions, prefilters to query object
-    //then return it
-    return query
-  }
- }, app)
+1. Install dependencies
 
 ```
-then in your Clientside UI code
-```js
-const searchkit = new SearchkitManager("/")
+npm install
 ```
 
+2. Update the Searchkit configuration in `index.js` with the `connection` and `search_settings` for your search experience.
 
-#### Alternative express router
-If you wish to get hold of an `express.Router` instance so you can configure the suburl and add specific express middleware; use as follows
+3. Start server. This will start the server on port 3000.
 
-```js
-var app = express()
-app.use(bodyParser.json())
-
-//...
-
-var searchkitRouter = SearchkitExpress.createRouter({
-  host:process.env.ELASTIC_URL || "http://localhost:9200",  
-  index:'movies',
-  maxSockets:500, // defaults to 1000
-  queryProcessor:function(query, req, res){
-    console.log(query)    
-    return query
-  }
- })
-app.use("/movie-search", searchkitRouter)
 ```
-then in your Clientside UI code
-```js
-const searchkit = new SearchkitManager("/movies-search")
+npm start
 ```
 
-#### Debugging
-To enable debugging, enable debugging by setting environment variable when starting your server.
-```sh
-DEBUG=SearchkitExpress node server.js
+4. Update the `url` in `@searchkit/instantsearch-client` configuration to point to the server
+
+```
+const searchClient = SearchkitInstantSearchClient({
+  url: "http://localhost:3000/api/search"
+})
 ```
--- index.js
+++ index.js
@@ -1,47 +1,166 @@
-var _ = require("lodash")
-var express = require("express")
-var debug = require("debug")("SearchkitExpress")
-var request = require("request")
+import express from "express";
+import Client from "@searchkit/api";
+import 'isomorphic-unfetch'
 
-var createSearchkitRouter = function(config) {
-  var router = express.Router()
-  config.queryProcessor = config.queryProcessor || _.identity
-  var requestClient = request.defaults({
-    pool: {
-      maxSockets: config.maxSockets || 1000
-    }
-  });
+const app = express();
 
-  var elasticRequest = function(url, body){
-    var fullUrl = config.host+ "/" + config.index + url;
-    debug("Start Elastic Request", fullUrl)
-    if(_.isObject(body)){
-      debug("Request body", body)
-    }
-    return requestClient.post({
-      url:fullUrl,
-      body:body,
-      json:_.isObject(body),
-      forever:true
-    }).on("response", function(response){
-      debug("Finished Elastic Request", fullUrl, response.statusCode)
-    }).on("error", function(response){
-      debug("Error Elastic Request", fullUrl,response.statusCode)
-    })
+const config = {
+  connection: {
+    host: 'https://commerce-demo.es.us-east4.gcp.elastic-cloud.com:9243',
+    apiKey: 'a2Rha1VJTUJMcGU4ajA3Tm9fZ0Y6MjAzX2pLbURTXy1hNm9SUGZGRlhJdw=='
+  },
+  search_settings: {
+    highlight_attributes: ['title'],
+    search_attributes: [{ field: 'title', weight: 3 }, 'actors', 'plot'],
+    result_attributes: ['title', 'actors', 'poster', 'plot'],
+    facet_attributes: [
+      'type',
+      { attribute: 'actors', field: 'actors.keyword', type: 'string' },
+      'rated',
+      { attribute: 'imdbrating', type: 'numeric', field: 'imdbrating' },
+      { attribute: 'metascore', type: 'numeric', field: 'metascore' }
+    ],
+    sorting: {
+      default: {
+        field: '_score',
+        order: 'desc'
+      },
+      _rated_desc: {
+        field: 'rated',
+        order: 'desc'
+      }
+    },
+    snippet_attributes: ['plot'],
+    query_rules: [
+      {
+        id: '1',
+        conditions: [[]],
+        actions: [
+          {
+            action: 'RenderFacetsOrder',
+            facetAttributesOrder: ['type', 'actors', 'rated', 'metascore']
+          }
+        ]
+      },
+      {
+        id: '2',
+        conditions: [
+          [
+            {
+              context: 'filterPresent',
+              values: [{ attribute: 'type', value: 'movie' }]
+            }
+          ]
+        ],
+        actions: [
+          {
+            action: 'RenderFacetsOrder',
+            facetAttributesOrder: ['type', 'actors', 'rated', 'imdbrating', 'metascore']
+          }
+        ]
+      },
+      {
+        id: '3',
+        conditions: [
+          [
+            {
+              context: 'query',
+              match_type: 'prefix',
+              value: 'star'
+            }
+          ]
+        ],
+        actions: [
+          {
+            action: 'PinnedResult',
+            documentIds: ['tt0111161']
+          }
+        ]
+      },
+      {
+        id: '4',
+        conditions: [
+          [
+            {
+              context: 'query',
+              match_type: 'exact',
+              value: 'episode'
+            }
+          ]
+        ],
+        actions: [
+          {
+            action: 'RenderUserData',
+            userData: '{ "title": "Episode" }'
+          },
+          {
+            action: 'RenderFacetsOrder',
+            facetAttributesOrder: ['type']
+          },
+          {
+            action: 'QueryRewrite',
+            query: 'tv episodes'
+          }
+        ]
+      },
+      {
+        id: '5',
+        conditions: [
+          [
+            {
+              context: 'filterPresent',
+              values: [{ attribute: 'type', value: 'movie' }]
+            }
+          ]
+        ],
+        actions: [
+          {
+            action: 'RenderUserData',
+            userData: '{ "title": "Movie has been selected" }'
+          },
+          {
+            action: 'PinnedResult',
+            documentIds: ['tt0468569']
+          }
+        ]
+      },
+      {
+        id: '6',
+        conditions: [
+          [
+            {
+              context: 'query',
+              value: 'movie',
+              match_type: 'exact'
+            }
+          ]
+        ],
+        actions: [
+          {
+            action: 'QueryBoost',
+            query: 'actors:"Dan Aykroyd" OR actors:"Charlie Sheen"',
+            weight: 2
+          },
+          {
+            action: 'QueryFilter',
+            query: 'type:"movie"'
+          }
+        ]
+      }
+    ]
   }
-  router.post("/_search", function(req, res){
-    var queryBody = config.queryProcessor(req.body || {}, req, res)
-    elasticRequest("/_search", queryBody).pipe(res)
-  });
-
-  return router
 }
+ 
+const apiClient = Client(config);
 
-var searchkitExpress = function(config, expressApp){
-  var router = createSearchkitRouter(config)
-  expressApp.use("/", router)
-}
+app.use(express.json());
+ 
+app.post("/api/search", async function (req, res) {
+  const response = await apiClient.handleRequest(req.body);
+  res.send(response);
+});
+ 
 
-searchkitExpress.createRouter = createSearchkitRouter
-
-module.exports = searchkitExpress
+app.listen(3001, () => {
+  console.log("Server running on port 3000");
+});
